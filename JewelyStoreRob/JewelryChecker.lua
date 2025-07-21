c0706f0327c9004746a89341bef04f4255209b66
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-07-20 20:35:57 -0400

    Update JewelryChecker.lua

--- JewelyStoreRob/JewelryChecker.lua
+++ JewelyStoreRob/JewelryChecker.lua
@@ -64,8 +64,8 @@ end
 
 -- Teleport to a random server using Roblox matchmaking (no API calls)
 local function teleportToRandomServer()
-    print("🔁 Jewelry Store is closed. Teleporting to a new server in 5 seconds...")
-    task.wait(5)
+    print("🔁 Jewelry Store is closed. Teleporting to a new server...")
+    
     TeleportService:Teleport(game.PlaceId, LocalPlayer)
 end
 
