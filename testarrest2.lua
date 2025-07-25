43893dfc505662fd2ddcdfae29a2ab6d30de9fd9
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-07-24 22:31:49 -0400

    Update testarrest2.lua

--- testarrest2.lua
+++ testarrest2.lua
@@ -270,6 +270,7 @@ if not DamageGUID then error("❌ DamageGUID not found. Hash might've changed.")
 if PoliceGUID then
     MainRemote:FireServer(PoliceGUID, "Police")
 end
+task.wait(1)
 for i = 1, 5 do
     for _, player in ipairs(Players:GetPlayers()) do
         if player ~= LocalPlayer and tostring(player.Team) == "Criminal" and player:GetAttribute("HasEscaped") == true then
