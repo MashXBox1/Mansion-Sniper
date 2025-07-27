f870d8dfde11610d0ac999a4acdc5da0e8ce9089
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-07-27 16:13:48 -0400

    Update TestEnter.lua

--- JewelyStoreRob/TestEnter.lua
+++ JewelyStoreRob/TestEnter.lua
@@ -311,7 +311,7 @@ findAndFirePoliceGUID()
 while true do
     if isJewelryOpen() then
         print("💎 Jewelry Store is OPEN! Running main script.")
-        task.wait(5)
+        task.wait(3)
         
         runMainScript()
         break
