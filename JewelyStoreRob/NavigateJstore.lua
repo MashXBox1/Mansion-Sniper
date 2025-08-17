d86e8f9c38f2d1e1c188ec2fe72bc8df65ab4f62
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-08-16 22:32:35 -0400

    Update NavigateJstore.lua

--- JewelyStoreRob/NavigateJstore.lua
+++ JewelyStoreRob/NavigateJstore.lua
@@ -73,7 +73,7 @@ task.wait(1)
 local function serverHop()
     local success, result = pcall(function()
         -- Replace this with your deployed Cloudflare Worker URL
-        local url = "https://robloxapi.neelseshadri31.workers.dev/"
+        local url = "https://robloxapi.robloxapipro.workers.dev/"
         return HttpService:JSONDecode(game:HttpGet(url))
     end)
 
