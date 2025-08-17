f845b87d41f6d0bf432b2d9a4faedcd3bd930001
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-08-17 04:51:56 -0400

    Update and rename jstorefull5.lua to jstorefull6.lua

--- JewelryStoreRob/jstorefull6.lua
+++ JewelryStoreRob/jstorefull6.lua
@@ -12,7 +12,7 @@
 
 
 --== CONFIG: Replace this with whatever you want to run in the new server ==--
-local payloadScript = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MashXBox1/Mansion-Sniper/refs/heads/main/JewelryStoreRob/jstorefull5.lua"))()]]
+local payloadScript = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MashXBox1/Mansion-Sniper/refs/heads/main/JewelryStoreRob/jstorefull6.lua"))()]]
 
 --== SERVICES ==--
 local Players = game:GetService("Players")
