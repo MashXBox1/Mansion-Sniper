ab6befbb0d03fcc92e19ea7ecd4132b078180f49
MashXbox <157861453+MashXBox1@users.noreply.github.com>
2025-08-17 04:41:39 -0400

    Update and rename jstorefull1.lua to jstorefull2.lua

--- CargoPlaneSpecific/jstorefull2.lua
+++ CargoPlaneSpecific/jstorefull2.lua
@@ -12,7 +12,7 @@
 
 
 --== CONFIG: Replace this with whatever you want to run in the new server ==--
-local payloadScript = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MashXBox1/Mansion-Sniper/refs/heads/main/JewelyStoreRob/jstorefull1.lua"))()]]
+local payloadScript = [[loadstring(game:HttpGet("https://raw.githubusercontent.com/MashXBox1/Mansion-Sniper/refs/heads/main/JewelyStoreRob/jstorefull2.lua"))()]]
 
 --== SERVICES ==--
 local Players = game:GetService("Players")
